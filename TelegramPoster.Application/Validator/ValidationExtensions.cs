e3841c27d0e660c2fe61b32add2cfe15028f3797
Andrew <andvolozhaninov@apri-code.ru>
2024-07-01 22:40:35 +0500

    Сделал AssertFound, возвращающий bool

--- TelegramPoster.Application/Validator/ValidationExtensions.cs
+++ TelegramPoster.Application/Validator/ValidationExtensions.cs
@@ -3,11 +3,13 @@
 namespace TelegramPoster.Application.Validator;
 public static class ValidationExtensions
 {
-    public static void AssertFound<T>(this T? value, ModelStateDictionary modelState) where T : class
+    public static bool AssertFound<T>(this T? value, ModelStateDictionary modelState) where T : class
     {
         if (value == null)
         {
             modelState.AddModelError(typeof(T).Name, "Запрашиваемый ресурс не найден.");
+            return false;
         }
+        return true;
     }
 }
